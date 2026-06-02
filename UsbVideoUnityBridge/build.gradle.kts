feb03c7187461115ddc20a6d965a79fe554c6d28
Arthur Pointeau <arthur.pointeau@protonmail.com>
2026-06-02 13:39:05 +0200

    fix depenencies for sdk 34

--- UsbVideoUnityBridge/build.gradle.kts
+++ UsbVideoUnityBridge/build.gradle.kts
@@ -32,8 +32,8 @@ android {
 dependencies {
   compileOnly(files("libs/classes.jar"))
   implementation("androidx.appcompat:appcompat:1.6.1")
-  implementation("androidx.core:core-ktx:1.18.0")
-  implementation("com.google.android.material:material:1.14.0")
+  implementation("androidx.core:core-ktx:1.13.1")
+  implementation("com.google.android.material:material:1.12.0")
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
   androidTestImplementation("androidx.test.ext:junit:1.3.0")
